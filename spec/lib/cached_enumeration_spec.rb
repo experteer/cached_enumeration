f97089ee2b5bab3444f98ffefff19d33b8a96f28
Michael Sauter <michael.sauter@experteer.com>
2015-08-03 17:17:19 +0200

    Convert specs to RSpec 3.3.2 syntax with Transpec
    This conversion is done by Transpec 3.1.1 with the following command:
        transpec
    
    * 29 conversions
        from: obj.should
          to: expect(obj).to
    
    * 25 conversions
        from: == expected
          to: eq(expected)
    
    * 12 conversions
        from: obj.should_not_receive(:message)
          to: expect(obj).not_to receive(:message)
    
    * 6 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 3 conversions
        from: lambda { }.should
          to: expect { }.to
    
    * 2 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/lib/cached_enumeration_spec.rb
+++ spec/lib/cached_enumeration_spec.rb
@@ -27,13 +27,13 @@ describe 'simple caching' do
 
   context "cache_enumeration?" do
     it "should return the rigth value" do
-      @klass.should_not be_cache_enumeration
+      expect(@klass).not_to be_cache_enumeration
       @klass.cache_enumeration.cache!
-      @klass.should be_cache_enumeration
+      expect(@klass).to be_cache_enumeration
     end
     it "should not cache if not activated" do
       @klass.all
-      @klass.should_not be_cache_enumeration
+      expect(@klass).not_to be_cache_enumeration
     end
   end
 
@@ -45,29 +45,29 @@ describe 'simple caching' do
 
     it "should fire db queries if all is modified" do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_receive(:exec_query).and_call_original
-      @klass.where("name in ('one','two')").all.size.should == 2
+      expect(@klass.connection).to receive(:exec_query).and_call_original
+      expect(@klass.where("name in ('one','two')").all.size).to eq(2)
     end
 
     it 'should fire db queries if all has parameters' do
       @klass.cache_enumeration.cache!
       #@klass.connection.should_receive(:exec_query)..and_call_original
-      @klass.where("name = 'one'").all.size.should == 1
+      expect(@klass.where("name = 'one'").all.size).to eq(1)
     end
 
     it 'should fire db queries if all with parameters is used through find(:all)' do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_receive(:exec_query).and_call_original
-      @klass.where("name = 'one'").all.size.should == 1
+      expect(@klass.connection).to receive(:exec_query).and_call_original
+      expect(@klass.where("name = 'one'").all.size).to eq(1)
     end
       
     it 'should fire db queries if all with select parameter is used through find(:all)' do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_receive(:exec_query).and_call_original
+      expect(@klass.connection).to receive(:exec_query).and_call_original
       entry = @klass.select("id, name").all.first
-      lambda {
+      expect {
         entry.other
-      }.should raise_error(ActiveModel::MissingAttributeError, 'missing attribute: other')
+      }.to raise_error(ActiveModel::MissingAttributeError, 'missing attribute: other')
     end
       
   end
@@ -75,40 +75,40 @@ describe 'simple caching' do
   context 'first' do
     it 'should find the first entry' do
       @klass.cache_enumeration.cache!
-      @klass.first.should == one
+      expect(@klass.first).to eq(one)
     end
     it 'should allwo string order (and use cache)' do
       @klass.cache_enumeration(:order => "other").cache!
-      @klass.connection.should_not_receive(:exec_query)
+      expect(@klass.connection).not_to receive(:exec_query)
 
-      @klass.order('other').first.should == three
+      expect(@klass.order('other').first).to eq(three)
     end
     it 'should allow hash condition (and use cache)' do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_not_receive(:exec_query)
+      expect(@klass.connection).not_to receive(:exec_query)
 
-      @klass.where(:name => 'three').first.should == three
+      expect(@klass.where(:name => 'three').first).to eq(three)
     end
     it 'should allow string conditions (and ask db)' do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_receive(:exec_query).and_call_original
-      @klass.where("other = 'drei'").first.should == three
+      expect(@klass.connection).to receive(:exec_query).and_call_original
+      expect(@klass.where("other = 'drei'").first).to eq(three)
     end
 
     it 'should allow hash conditions in first (and use cache)' do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_not_receive(:exec_query)
-      @klass.where(:name => 'three' ).first.should == three
+      expect(@klass.connection).not_to receive(:exec_query)
+      expect(@klass.where(:name => 'three' ).first).to eq(three)
     end
 
     it 'should allow conditions for first (and use db)' do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_receive(:exec_query).and_call_original
-      @klass.where("other = 'drei'").first.should == three
+      expect(@klass.connection).to receive(:exec_query).and_call_original
+      expect(@klass.where("other = 'drei'").first).to eq(three)
     end
     it 'should allow conditions for first (and use db)' do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_receive(:exec_query).and_call_original
+      expect(@klass.connection).to receive(:exec_query).and_call_original
       expect(@klass.where("name = 'three'").first).not_to be_nil
     end
   end
@@ -120,40 +120,40 @@ describe 'simple caching' do
 
     it 'should find objects providing id' do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_not_receive(:exec_query)
+      expect(@klass.connection).not_to receive(:exec_query)
 
-      @klass.find(one.id).id.should == one.id
-      @klass.find(one.id).frozen?().should eq(true)
-      @klass.find([one.id])[0].id.should == one.id
-      @klass.find([]).size.should == 0
+      expect(@klass.find(one.id).id).to eq(one.id)
+      expect(@klass.find(one.id).frozen?()).to eq(true)
+      expect(@klass.find([one.id])[0].id).to eq(one.id)
+      expect(@klass.find([]).size).to eq(0)
 
-      lambda { @klass.find(0) }.should raise_error(ActiveRecord::RecordNotFound)
-      lambda { @klass.find(nil) }.should raise_error(ActiveRecord::RecordNotFound)
+      expect { @klass.find(0) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect { @klass.find(nil) }.to raise_error(ActiveRecord::RecordNotFound)
     end
 
     it "should find an array of object ids (and hit cache)" do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_not_receive(:exec_query)
+      expect(@klass.connection).not_to receive(:exec_query)
 
-      @klass.find([one.id, three.id]).collect { |item| item.id }.should == [one.id, three.id]
+      expect(@klass.find([one.id, three.id]).collect { |item| item.id }).to eq([one.id, three.id])
     end
 
     it 'should find_by' do
       @klass.cache_enumeration.cache!
-      @klass.connection.should_not_receive(:exec_query)
+      expect(@klass.connection).not_to receive(:exec_query)
 
-      @klass.find_by(:id => one.id).id.should == one.id
-      @klass.find_by(:id => one.id.to_s).id.should == one.id
-      @klass.find_by(:id => 0).should be_nil
+      expect(@klass.find_by(:id => one.id).id).to eq(one.id)
+      expect(@klass.find_by(:id => one.id.to_s).id).to eq(one.id)
+      expect(@klass.find_by(:id => 0)).to be_nil
     end
 
     it 'should find objects by_name' do
       one
       @klass.cache_enumeration.cache!
-      @klass.connection.should_not_receive(:exec_query)
+      expect(@klass.connection).not_to receive(:exec_query)
 
-      @klass.find_by(:name => 'one').id.should == one.id
-      @klass.find_by(:name => 'no such name').should be_nil
+      expect(@klass.find_by(:name => 'one').id).to eq(one.id)
+      expect(@klass.find_by(:name => 'no such name')).to be_nil
     end
 
   end
@@ -161,43 +161,43 @@ describe 'simple caching' do
   context "multiple keys" do
     it 'it should store by multiple keys (hashing)' do
       @klass.cache_enumeration(:hashed => ['id', 'other', 'name']).cache!
-      @klass.connection.should_not_receive(:exec_query)
-      @klass.find_by(:other => 'eins').id.should ==one.id
-      @klass.find_by(:name => 'one').id.should ==one.id
+      expect(@klass.connection).not_to receive(:exec_query)
+      expect(@klass.find_by(:other => 'eins').id).to eq(one.id)
+      expect(@klass.find_by(:name => 'one').id).to eq(one.id)
     end
   end
   context "sorting of all" do
     it 'should sort by option' do
       @klass.cache_enumeration(:order => 'name').cache!
-      @klass.connection.should_not_receive(:exec_query)
+      expect(@klass.connection).not_to receive(:exec_query)
 
-      @klass.order("name").all.collect { |item| item.id }.should == [one.id, three.id, two.id]
+      expect(@klass.order("name").all.collect { |item| item.id }).to eq([one.id, three.id, two.id])
     end
 
   end
   context "constantize" do
     it "should constantize name by default" do
-      @klass.cache_enumeration.options[:constantize].should == 'name'
+      expect(@klass.cache_enumeration.options[:constantize]).to eq('name')
     end
     it "should without no preloading" do
 
       @klass.cache_enumeration
-      @klass::ONE.id.should == one.id
+      expect(@klass::ONE.id).to eq(one.id)
     end
 
     it 'should constantize other fields' do
       @klass.cache_enumeration(:constantize => 'other').cache!
-      @klass.cache_enumeration.options[:constantize].should == 'other'
-      @klass.connection.should_not_receive(:exec_query)
+      expect(@klass.cache_enumeration.options[:constantize]).to eq('other')
+      expect(@klass.connection).not_to receive(:exec_query)
 
-      @klass::EINS.id.should == one.id
+      expect(@klass::EINS.id).to eq(one.id)
     end
 
     it "should contantize by lambda" do
       @klass.cache_enumeration(:constantize => lambda { |model| model.other }).cache!
-      @klass.connection.should_not_receive(:exec_query)
+      expect(@klass.connection).not_to receive(:exec_query)
 
-      @klass::EINS.id.should == one.id
+      expect(@klass::EINS.id).to eq(one.id)
     end
   end
 
