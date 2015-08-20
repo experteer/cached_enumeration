d6fd68dc092839beecdbe8b5fbbfc3325c9764e3
Michael Sauter <michael.sauter@experteer.com>
2015-08-20 15:57:22 +0200

    Remove some caching which will not work with Rails 4.2
    Rails 4.2 will have "AdequateRecord", which prevents us from
    caching certain calls (unless we wanted to override find itself).
    
    Therefore, we are trying the impact of this ahead of time in Rails 3.2 to see
    what performance is like if we do not cache a few things. What we're trying
    to see is the worst-case scenario. Request time will likely drop in Rails 4.2
    again since "AdequateRecord" is faster than ActiveRecord in Rails 3.2 for many
    scenarios which were cached previously.
    
    Before this change, we have the following scenario:
    
    * +all+ is cached (with the alias +cached_all+)
    * +where+ is not cached
    * +by_xyz+ is cached (depending on all_with_....)
    * +find_by_xyz+ is cached (depending on all_with_...)
    * +find+ is cached (depending on find_one_with_.../find_some_with...)
    * +first+ is cached (depending on find_first_with_....)
    * Constants are cached (depending on all_with_...)
    
    After this change, we only cache constants, +all+ and +by_xyz+.
    
    In Rails 4.2, we're going to implement +by_xyz+ by caching +where+.
    We are dropping +find_by_xyz+ too since that is using "AdequateRecord" in
    Rails 4.2 by default.

--- spec/lib/association_spec.rb
+++ spec/lib/association_spec.rb
@@ -40,49 +40,4 @@ describe 'association caching' do
     Profile.delete_all
   end
 
-
-  let(:him) { Profile.create(:name => 'Him', :gender => Gender::MALE) }
-  let(:her) { Profile.create(:name => 'Her', :gender => Gender::FEMALE) }
-
-
-  context "should use cached when going over association" do
-
-    it 'should find the cached ones (no :include)' do
-      him
-      him.reload #to empty the assoc cache
-
-      Gender.connection.should_not_receive(:exec_query)
-      him.gender.name.should == 'male'
-    end
-
-=begin
-
-does not work: Profile.includes(:gender).all creates two sql statements
-to load profiles AND genders associated to them. The 2nd one (for genders)
-does not run through standard finders but is done in the depth of AR.
-So caching does not work here.
-
-Possible improvements:
-* intercept `all' for ALL AR models and take out inclusions where the
-  model is cached (restricted to simple cases of belongs_to)
-* find an entry point deeper in AR where associations are loaded and
-  modify that to consider model caching
-
-Until then, one should just leave out cached models in inclusions,
-though that makes switching caching on or off for a model quite difficult.
-
-    it "should take the :include from the cache" do
-      #logged do
-      him;her
-        ActiveRecord::Base.connection.should_receive(:exec_query).once.and_call_original
-        all=Profile.includes(:gender).all
-        ActiveRecord::Base.connection.should_not_receive(:exec_query)
-        him.gender.name.should == 'male'
-        her.gender.name.should == 'female'
-      #end
-    end
-=end
-
-
-  end
 end
